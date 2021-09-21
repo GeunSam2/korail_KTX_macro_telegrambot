2fbb19f759d6332ab9c68458e4d6f8f82e308820
GeunSam2 <rootiron96@gmail.com>
2021-09-21 20:12:20 +0900

    rollback

---
+++ source/telegramBot/korail2/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    korail2
+    ~~~~~~~
+
+    Korail (www.letskorail.com) wrapper for Python.
+
+    :copyright: (c) 2014 by Taehoon Kim.
+    :license: BSD, see LICENSE for more details.
+"""
+from .korail2 import Korail, Passenger, AdultPassenger, ChildPassenger, SeniorPassenger, TrainType, ReserveOption
+from .korail2 import KorailError, NeedToLoginError, SoldOutError, NoResultsError
+
+__all__ = ['Korail', 'Passenger', 'AdultPassenger', 'ChildPassenger', 'SeniorPassenger', 'TrainType', 'ReserveOption',
+           'KorailError', 'NeedToLoginError', 'SoldOutError', 'NoResultsError']
