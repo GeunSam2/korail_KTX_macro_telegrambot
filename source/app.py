b2fd62aa967f30af7e67c147c26e87d2761fdb9b
GeunSam2 <rootiron96@gmail.com>
2022-12-31 02:57:36 +0900

    add debug

--- source/app.py
+++ source/app.py
@@ -11,5 +11,5 @@ api = Api(application)
 api.add_resource(Index, '/telebot')
 
 if __name__ == '__main__':
-
-    application.run(host='0.0.0.0', port=8080, threaded=True)
+    
+    application.run(debug = True, host='0.0.0.0', port=8080, threaded=True)
